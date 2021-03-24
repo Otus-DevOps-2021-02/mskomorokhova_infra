eb502868dafa66c023d8ea100dcb714772d59af2
Marina Skomorokhova <mskomorokhova@diasoft.ru>
2021-03-24 17:59:17 +0300

    update test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y install git
-cd /home/yc-user
+sudo apt install git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
